f8ef9444cd4655c310860d659951c3f909d4c497
werlen-nevio <LordNevi@outlook.com>
2024-05-08 16:12:56 +0200

    Add Filter so only with a token you can access /api

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -104,5 +104,7 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+        'jwt' => ['before' => ['api', 'api/*']],
+    ];
 }
