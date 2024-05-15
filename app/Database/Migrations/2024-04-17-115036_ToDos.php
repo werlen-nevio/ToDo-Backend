a5201b2752c7e0a9cddee1434b0d925d36fbc596
Tobias <tobias.holzer@edu.vs.ch>
2024-05-15 15:45:21 +0200

    new migration-script

--- app/Database/Migrations/2024-04-17-115036_ToDos.php
+++ app/Database/Migrations/2024-04-17-115036_ToDos.php
@@ -8,18 +8,23 @@ class ToDos extends Migration
 {
     public function up()
     {
-        $this->db->query('CREATE TABLE todos (
-            id INT(11) NOT NULL AUTO_INCREMENT,
-            todo_brand VARCHAR(255),
-            todo_name VARCHAR(255),
-            color_hex VARCHAR(6),
-            comments TEXT,
-            todo_type_id INT(11),
-            created_at DATETIME,
-            updated_at DATETIME,
-            deleted_at DATETIME,
-            PRIMARY KEY (id)
-        )');
+
+		$this->db->query('CREATE TABLE todos (
+        todo_id INT(11) NOT NULL AUTO_INCREMENT,
+		todo_name VARCHAR(255),
+		todo_description TEXT,
+		todo_date DATETIME,
+		todo_status VARCHAR(255),
+		todo_category VARCHAR(255),
+		category_id INT(11) NOT NULL AUTO_INCREMENT,
+		category_name VARCHAR(255),		
+		date DATETIME,
+		action VARCHAR(255),
+		table VARCHAR(255),
+		data_json_old VARCHAR(255),
+		data_json_new VARCHAR(255),
+    }
+	)');
     }
 
     public function down()
